3955088b3a1eb11b8ec721bd6652b6733b996b36
Muhammad Abdullah <m.abdullah.fuf@gmail.com>
2024-10-16 18:50:25 +0500

    add confromation on update and delete using livewire

--- resources/views/livewire/includes/todo-card.blade.php
+++ resources/views/livewire/includes/todo-card.blade.php
@@ -31,7 +31,7 @@
                         d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                 </svg>
             </button>
-            <button wire:click="delete({{ $todo->id }})"
+            <button wire:click="delete({{ $todo->id }})" wire:confirm="Are you sure want to delete this?"
                 class="text-sm text-red-500 font-semibold rounded hover:text-teal-800 mr-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-4 h-4">
@@ -44,7 +44,7 @@
     <span class="text-xs text-gray-500"> {{ $todo->created_at }} </span>
     <div class="mt-3 text-xs text-gray-700">
         @if ($editingtodoID === $todo->id)
-            <button wire:click="update" class="mt-3 px-4 py-2 bg-teal-500 text-white font-semibold rounded hover:bg-teal-600">Update</button>
+            <button wire:click="update" wire:confirm="Are you sure want to update this?" class="mt-3 px-4 py-2 bg-teal-500 text-white font-semibold rounded hover:bg-teal-600">Update</button>
             <button wire:click="cancelEdit" class="mt-3 px-4 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600">Cancel</button>
         @endif
     </div>
